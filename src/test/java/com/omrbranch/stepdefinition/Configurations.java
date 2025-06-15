9d25c65745e646696716c4bc53c772401a607f5f
mathi123-rs <mathiyazhaganr1997@gmail.com>
2025-06-15 12:58:19 +0530

    adding class and feature file

---
+++ src/test/java/com/omrbranch/stepdefinition/Configurations.java
@@ -0,0 +1,15 @@
+package com.omrbranch.stepdefinition;
+
+import java.lang.module.Configuration;
+
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class Configurations {
+
+	
+
+}
+
